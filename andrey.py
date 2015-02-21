4dbab2cb1ba694b3057e3d5c0d3b577a3dfaa09c
Eric Stein <toba@des.truct.org>
2015-02-21 02:01:35 -0600

    to and from dictionary

--- andrey.py
+++ andrey.py
@@ -1,84 +1,138 @@
 import random
 
-class ChainLink(object) :
-	def __init__(self, tokens) :
-		self.n = 0
-		self.tokens = tokens
 
-	def incr(self) :
-		self.n += 1
+class ChainLink(object):
 
-class Chain(object) :
-	def __init__(self) :
-		self.n = 0
-		self.links = dict()
+    def __init__(self, tokens, n=0):
+        self.n = n
+        self.tokens = tokens
 
-	def add(self, tokens) :
-		self.n += 1
-		if tokens not in self.links :
-			self.links[tokens] = ChainLink(tokens)
-		self.links[tokens].incr()
+    def incr(self):
+        self.n += 1
 
-	def choose(self) :
-		seek = random.randrange(0, self.n)
-		for link in self.links.values() :
-			seek -= link.n
-			if seek <= 0 :
-				return link.tokens
-
-class Markov(object) :
-	"""
-	M is the number of tokens per inbound prediction
-	N is the number of tokens in the outbound output
-	"""
-	
-
-	def __init__(self, m, n) :
-		self.m = m
-		self.n = n
-		self.state = {}
+    def __str__(self):
+        return '  %d: %s' % (self.n, ' '.join(self.tokens))
 
 
+class Chain(object):
 
-	def tokenize(self, tokens) :
-		if tokens.__class__ == str :
-			tokens = tuple(tokens.split(' '))
-		return tokens
+    def __init__(self):
+        self.n = 0
+        self.links = dict()
 
-	def teach(self, tokens) :
-		tokens = self.tokenize(tokens)
-		for i in range(len(tokens) + 1 - self.m - self.n) :
-			mi = i
-			ni = mi + self.m
-			inbound_key = tokens[mi:mi+self.m]
-			outbound_key = tokens[ni:ni+self.n]
+    def __str__(self):
+        return '%d total:\n%s' % (self.n, '\n'.join([str(cl) for cl in self.links.values()]))
 
-			if inbound_key not in self.state :
-				self.state[inbound_key] = Chain()
-			self.state[inbound_key].add(outbound_key)
+    def add(self, tokens):
+        self.n += 1
+        if tokens not in self.links:
+            self.links[tokens] = ChainLink(tokens)
+        self.links[tokens].incr()
 
-	# if continued > 0, then we assume that n >= m
-	def _choose(self, tokens, continued=0) :
-		tokens = self.tokenize(tokens)
-		ns = range(len(tokens) + 1 - self.m)
-		random.shuffle(ns)
-		for i in ns :
-			ik = tokens[i:i+self.m]
-			if ik in self.state :
-				c = self.state[ik].choose()
-				if c :
-					if continued < 1 :
-						return c
-					else :
-						nc = self._choose(c[-self.m:], continued=continued-1)
-						if nc is None :
-							return c
-						return c + nc
+    def addlink(self, chainlink):
+        self.n += chainlink.n
+        self.links[chainlink.tokens] = chainlink
 
-	def choose(self, *a, **kw) :
-		return self.detokenize(self._choose(*a, **kw))
+    def choose(self):
+        seek = random.randrange(0, self.n)
+        for link in self.links.values():
+            seek -= link.n
+            if seek <= 0:
+                return link.tokens
 
-	def detokenize(self, tokens) :
-		if tokens is None :
-			return None
-		return ' '.join(list(tokens))
+    def tolist(self):
+        return [
+            [cl.n, list(cl.tokens)]
+            for
+            cl
+            in
+            self.links.values()
+        ]
+
+    @classmethod
+    def fromlist(cls, d):
+        o = cls()
+
+        for cll in d:
+            o.addlink(ChainLink(tuple(cll[1]), n=cll[0]))
+        return o
+
+
+class Markov(object):
+
+    """
+    M is the number of tokens per inbound prediction
+    N is the number of tokens in the outbound output
+    """
+
+    def __init__(self, m, n):
+        self.m = m
+        self.n = n
+        self.state = {}
+
+    def __str__(self):
+        return 'markov %d -> %d\n%s' % (self.m, self.n, '\n'.join(['%s: %s' % (self.detokenize(itok), c) for (itok, c) in self.state.items()]))
+
+    def todict(self):
+        return {
+            'm': self.m,
+            'n': self.n,
+            'chains': [
+                [list(intokens), chain.tolist()]
+                for
+                (intokens, chain)
+                in
+                self.state.items()
+            ]
+        }
+
+    @classmethod
+    def fromdict(cls, d):
+        o = cls(d['m'], d['n'])
+        for (intokenl, cl) in d['chains']:
+            tokens = tuple(intokenl)
+            o.state[tokens] = Chain.fromlist(cl)
+        return o
+
+    def tokenize(self, tokens):
+        if tokens.__class__ == str:
+            tokens = tuple(tokens.split(' '))
+        return tokens
+
+    def teach(self, tokens):
+        tokens = self.tokenize(tokens)
+        for i in range(len(tokens) + 1 - self.m - self.n):
+            mi = i
+            ni = mi + self.m
+            inbound_key = tokens[mi:mi + self.m]
+            outbound_key = tokens[ni:ni + self.n]
+
+            if inbound_key not in self.state:
+                self.state[inbound_key] = Chain()
+            self.state[inbound_key].add(outbound_key)
+
+    # if continued > 0, then we assume that n >= m
+    def _choose(self, tokens, continued=0):
+        tokens = self.tokenize(tokens)
+        ns = range(len(tokens) + 1 - self.m)
+        random.shuffle(ns)
+        for i in ns:
+            ik = tokens[i:i + self.m]
+            if ik in self.state:
+                c = self.state[ik].choose()
+                if c:
+                    if continued < 1:
+                        return c
+                    else:
+                        nc = self._choose(c[-self.m:], continued=continued - 1)
+                        if nc is None:
+                            return c
+                        return c + nc
+
+    def choose(self, *a, **kw):
+        return self.detokenize(self._choose(*a, **kw))
+
+    def detokenize(self, tokens):
+        if tokens is None:
+            return None
+        return ' '.join(list(tokens))
--- tests/test_chain.py
+++ tests/test_chain.py
@@ -1,14 +1,34 @@
 import unittest
 import andrey
 
-class MarkovTests(unittest.TestCase) :
-    def test_1chain(self) :
+
+class MarkovTests(unittest.TestCase):
+
+    def test_1chain(self):
         m = andrey.Markov(1, 1)
         m.teach('a b c d')
         self.assertEquals('b c', m.choose('a', continued=1))
 
-    def test_2chain(self) :
+    def test_2chain(self):
         m = andrey.Markov(2, 2)
         m.teach('a b c d')
         m.teach('b c ddd ddd')
         self.assertEquals('c d', m.choose('a b', continued=0))
+
+    def test_todict(self):
+        m = andrey.Markov(1, 1)
+        m.teach('a b c')
+        self.assertEquals({
+            'm': 1,
+            'n': 1,
+            'chains': [
+                [['a'], [[1, ['b']]]],
+                [['b'], [[1, ['c']]]],
+            ]
+        }, m.todict())
+
+    def test_1chain_simplerestore(self):
+        m = andrey.Markov(1, 1)
+        m.teach('a b c d')
+        m2 = andrey.Markov.fromdict(m.todict())
+        self.assertEquals('b c', m2.choose('a', continued=1))
