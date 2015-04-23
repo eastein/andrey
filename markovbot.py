24801f4de028825afb64e8ee8350d0cde409fa85
Eric Stein <toba@des.truct.org>
2015-04-22 22:29:05 -0500

    Add in --replace-probability and --word-filter

--- markovbot.py
+++ markovbot.py
@@ -15,8 +15,13 @@ SAVE_WINDOW = 300.0
 def wordprocess(s, repl=None):
     if repl is None:
         return s
-    if isinstance(repl, list):
-        repl = random.choice(repl)
+    repl_w = repl['w']
+    if 'p' in repl:
+        if random.random() > repl['p']:
+            return s
+
+    if isinstance(repl_w, list):
+        repl_w = random.choice(repl_w)
     keep = ['"', "'", ',', ';', ':', '!', '?', '.', 'ing', 's']
     rhs = ''
     found = True
@@ -27,7 +32,7 @@ def wordprocess(s, repl=None):
                 s = s[0:len(s) - len(k)]
                 rhs = '%s%s' % (k, rhs)
                 found = True
-    return '%s%s' % (repl, rhs)
+    return '%s%s' % (repl_w, rhs)
 
 
 class MarkovBot(mediorc.IRC):
@@ -70,11 +75,39 @@ class MarkovBot(mediorc.IRC):
         chan = e.target
         txt = e.arguments[0]
 
+        def output_filtering(r):
+            return ' '.join([wordprocess(s, repl=self.word_replace) for s in r.split(' ')])
+
+        def generate(txt, attempts=1, acceptance_test=None):
+            def generate_inner(txt):
+                return self.m.choose(txt, continued=10)
+            if not acceptance_test:
+                return generate_inner(txt)
+            else:
+                for i in range(attempts):
+                    r = generate_inner(txt)
+                    if acceptance_test(r):
+                        return r
+
+        def filter_test(r):
+            if r is None:
+                return False
+            do_send = False
+            rlower = r.lower()
+            for word in self.word_filter:
+                if word.lower() in rlower:
+                    do_send = True
+                    break
+            return do_send
+        
         if random.random() < self.ratio:
-            r = self.m.choose(txt, continued=10)
+            if self.word_filter:
+                r = generate(txt, attempts=50, acceptance_test=filter_test)
+            else:
+                r = generate(txt)
+            
             if r:
-                r = ' '.join([wordprocess(s, repl=self.word_replace)
-                              for s in r.split(' ')])
+                r = output_filtering(r)
                 self.connection.privmsg(chan, r)
 
         self.m.teach(txt)
@@ -82,11 +115,12 @@ class MarkovBot(mediorc.IRC):
 
 
 class MarkovThread(mediorc.IRCThread):
-    def __init__(self, args, filename=None, ratio=None, word_replace=None):
+    def __init__(self, args, filename=None, ratio=None, word_replace=None, word_filter=None):
         self.a = args
         self.filename = filename
         self.ratio = ratio
         self.word_replace = word_replace
+        self.word_filter = word_filter
 
         load = False
         if filename:
@@ -105,6 +139,7 @@ class MarkovThread(mediorc.IRCThread):
         mb.filename = self.filename
         mb.ratio = self.ratio
         mb.word_replace = self.word_replace
+        mb.word_filter = self.word_filter
         return mb
 
 if __name__ == '__main__':
@@ -115,16 +150,25 @@ if __name__ == '__main__':
                       help='Probablity that the bot will attempt to respond to any given line of chat.')
     parser.add_option('--word-replace', dest='word_replace', default=None,
                       help='Comma separated (no whitespace) list of words to replace every word with (except for punctuation, plurals, gerunds...')
+    parser.add_option('--word-filter', dest='word_filter', default=None,
+                      help='Comma separated (no whitespace) list of words to filter by; only talk if one of these words is in the text the bot would say.')
+    parser.add_option('--replace-probability', dest='replace_probability', default=1.0, help="When word replace in use, replace words with only this probability.")
 
     (options, args) = parser.parse_args()
 
     word_replace = options.word_replace
     if word_replace is not None:
-        word_replace = word_replace.split(',')
+        word_replace = {
+            'w' : word_replace.split(','),
+            'p' : float(options.replace_probability)
+        }
+    word_filter = options.word_filter
+    if word_filter is not None:
+        word_filter = word_filter.split(',')
 
     try:
         s = MarkovThread(args, filename=options.file,
-                         ratio=float(options.ratio), word_replace=word_replace)
+                         ratio=float(options.ratio), word_replace=word_replace, word_filter=word_filter)
     except IndexError:
         print 'Bad parameters. For usage, use markovboy.py -h.'
         sys.exit(1)
