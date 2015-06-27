4230f8bc5ab1025ba3a32ea29c8e52dbc4e3b636
Eric Stein <toba@des.truct.org>
2015-06-26 19:31:25 -0500

    Take in files from stdin and learn into whatever markov file is given.

---
+++ markov_train.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import persist
+import sys
+
+if __name__ == '__main__':
+    f, t = (2, 3)
+    try:
+        f, t = (int(sys.argv[2]), int(sys.argv[3]))
+    except IndexError:
+        pass
+
+    fn = sys.argv[1]
+
+    print '%s %d %d' % (fn, f, t)
+
+    m = persist.PersistedMarkov.restore(fn, f, t)
+
+    for line in sys.stdin:
+        line = line.strip()
+        m.teach(line)
+
+    m.save(fn)
