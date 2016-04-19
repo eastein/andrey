e546acda8e382deed2414d612479886a085f51b2
Eric Stein <toba@des.truct.org>
2016-04-18 22:30:23 -0500

    also fixing pypi stuff...

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from distutils.core import setup
 
 setup(name='andrey',
-      version='0.0',
-      description='A markov chain lib and IRC bot',
+      version='0.1.0',
+      description='A markov chain library',
       author='Eric Stein',
       url='https://github.com/eastein/andrey',
       packages=['andrey'],
