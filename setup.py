8ac3bea2f5b1aa19ee70ffcd48de00b3fcffcf8e
Eric Stein <toba@des.truct.org>
2016-10-06 21:37:52 -0500

    installable & remove irc dep from requirements

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='andrey',
+      version='0.1.0',
+      description='Andrey - Markov Chain library',
+      author='Eric Stein',
+      author_email='toba@des.truct.org',
+      url='https://github.com/eastein/andrey/',
+      packages=['andrey'],
+      install_requires=['msgpack-python']
+     )
